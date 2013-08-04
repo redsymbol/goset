11d693aedcf71d60a158d822d0b72d25384836d0
Aaron Maxwell <amax@redsymbol.net>
2013-08-04 10:37:54 -0700

    Document types, functions and methods so far

--- src/set/set.go
+++ src/set/set.go
@@ -5,6 +5,26 @@ import (
 	"sort"
 )
 
+/*
+Basic set type. Create new objects with NewSet.
+
+Not safe for concurrent access, so you must wrap operations with a
+lock if accessing from multiple goroutines.
+
+*/
+type Set struct {
+	items map[interface{}]*struct{}
+}
+
+/*
+Create a new Set object. Arguments ITEMS... will be the initial members of the set.
+
+  Examples:
+     emptySet := NewSet()
+     intsSet  := NewSet(7, 12, 42)
+     mixedSet := NewSet("foo", "bar", 42, 2.818)
+
+*/
 func NewSet(items ...interface{}) *Set {
 	s := Set{
 		make(map[interface{}]*struct{}),
@@ -15,28 +35,44 @@ func NewSet(items ...interface{}) *Set {
 	return &s
 }
 
-type Set struct {
-	items map[interface{}]*struct{}
-}
+/*
+Check whether item is contained in the set.
 
+*/
 func (s *Set) Contains(item interface{}) bool {
 	_, ok := s.items[item]
 	return ok
 }
 
+/*
+Add an item to the set.
+
+If the item already exists inside the set, do nothing.
+
+*/
 func (s *Set) Add(item interface{}) {
 	s.items[item] = nil
 }
 
-/* Delete the item from the set, if it's in.
-   If the item is not in the set, do nothing.
+/*
+Delete the item from the set, if it's in.
+
+If the item is not in the set, do nothing.
+
+See also Remove.
+
 */
 func (s *Set) Discard(item interface{}) {
 	delete(s.items, item)
 }
 
-/* Delete the item from the set, if it's in.
-   If the item is not in the set, panic.
+/*
+Delete the item from the set, if it's in.
+
+If the item is not in the set, panic.
+
+See also Discard.
+
 */
 func (s *Set) Remove(item interface{}) {
 	if !s.Contains(item) {
@@ -45,10 +81,18 @@ func (s *Set) Remove(item interface{}) {
 	delete(s.items, item)
 }
 
+/*
+Get the number of items contained in the set.
+
+*/
 func (s *Set) Len() int {
 	return len(s.items)
 }
 
+/*
+Create a new set containing those elements in both this AND another set.
+
+*/
 func (s *Set) Intersect(other *Set) *Set {
 	newset := NewSet()
 	for item, _ := range s.items {
@@ -59,6 +103,10 @@ func (s *Set) Intersect(other *Set) *Set {
 	return newset
 }
 
+/*
+Create a new set containing those elements in this set OR another set.
+
+*/
 func (s *Set) Union(other *Set) *Set {
 	newset := NewSet()
 	for item, _ := range s.items {
@@ -70,6 +118,14 @@ func (s *Set) Union(other *Set) *Set {
 	return newset
 }
 
+/*
+Create a slice consisting of the elements in the set.
+
+Useful if you need to use function requiring the set data in slice form.
+
+The order is undefined. See also Sorted.
+
+*/
 func (s *Set) Slice() []interface{} {
 	slice := make([]interface{}, len(s.items))
 	var ii int
@@ -80,6 +136,16 @@ func (s *Set) Slice() []interface{} {
 	return slice
 }
 
+/*
+Create a slice consisting of elements in the set, sorted.
+
+The set type allows mixed types, which raises questions about
+comparision.  What Sorted does is render each element as a string, and
+then sorts them lexicographically. If you need a different ordering -
+for example, your set contains just integers, and you want them sorted
+numerically - use Slice and then sort it directly.
+
+*/
 func (s *Set) Sorted() []string {
 	strslice := make([]string, len(s.items))
 	for ii, val := range s.Slice() {
